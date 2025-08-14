b8592816d4f141cf398cf4ebe736a55ee03bcf1e
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-08-14 16:46:29 +0530

    Added basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <main className="main-content">
-
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </main>
   );
 }
