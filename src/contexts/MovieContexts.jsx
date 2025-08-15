2f4b97a6fcf4b9bc95c3806aaff1c2702a23d1d3
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-08-15 14:42:39 +0530

    Favorites completed

--- src/contexts/MovieContexts.jsx
+++ src/contexts/MovieContexts.jsx
@@ -6,7 +6,7 @@ export const useMovieContext = () => useContext(MovieContext)
 
 export const MovieProvider = ({children}) => {
     const [favorites, setFavorites] = useState(() => {
-        // Initialize state from localStorage immediately
+        
         const storedFavs = localStorage.getItem("favorites")
         if (storedFavs) {
             try {
