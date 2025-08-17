128b2a8bd54c03a52ceb220f2b2fcd283a78bac6
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-08-17 18:18:45 +0530

    Watch Later title added

--- src/pages/WatchLater.jsx
+++ src/pages/WatchLater.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import MovieCard from "../components/MovieCard";
 import "../css/Watch_Later.css";
 import {useMovieContext} from "../contexts/MovieContexts";
-
+import { useNavigate } from "react-router-dom";
 function WatchLater() {
   const { watchLater } = useMovieContext();
+  const navigate = useNavigate();
 
   if (watchLater.length === 0) {
     return (
         <div className="watch-later">
-            <h2>
-                NO MOVIES ADDED TO WATCH LATER
-            </h2>
+          <h1 className="watch-later-title-nomovies" onClick={()=> navigate("/")}>MovieHUB🎬</h1>
+          <h2>
+              NO MOVIES ADDED TO WATCH LATER
+          </h2>
         </div>
     )
   }
 
   return (
     <div className="watch-later">
+      <h1 className="watch-later-title-nomovies" onClick={()=> navigate("/")}>MovieHUB🎬</h1>
       <h2>Watch Later</h2>
       <div className="movie-list">
         {watchLater.map((movie) => (
